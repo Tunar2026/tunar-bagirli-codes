4e2e009f483dc10c35b8ec4265b2d554b3161aad
Tunar2026 <bagirlitunar7@gmail.com>
2026-07-05 22:52:14 +0400

    JS 6 tasklar

---
+++ Dərs 53 JS/ev task/app.js
@@ -0,0 +1,173 @@
+//1
+
+let superQehreman = prompt("Ən sevdiyiniz superqəhrəmanın adını yazın:");
+
+switch (superQehreman) {
+    case "hörümçək adam":
+    console.log("Divarlara dırmaşa bilir.");
+    break;
+
+    case "betmen":
+    console.log(" Texnologiyadan istifadə edir.");
+    break;
+
+    case "supermen":
+    console.log("Uça bilir və çox güclüdür.");
+    break;
+
+    default:
+    console.log("Bu superqəhrəman siyahıda yoxdur.");
+}
+
+// 2
+
+let hava = prompt("Hava necədir? (günəşli, yağışlı, qarlı)");
+
+switch (hava) {
+    case "günəşli":
+    console.log("eynək taxın və qolu açıq kofta geyinə bilərsiniz.");
+    break;
+
+    case "yağışlı":
+    console.log("Çətir götür.");
+    break;
+
+    case "qarlı":
+    console.log("Qalın şalvar geyinin.");
+    break;
+
+    default:
+    console.log("Belə hava vəziyyəti tanınmadı.");
+}
+
+// 3
+
+let planet = prompt("Hansı planetə uçmaq istəyirsiniz? (mars, yupiter, yer)");
+
+switch (planet) {
+    case "mars":
+    console.log("Marsa maraqlı səyahət başlayır!");
+    break;
+
+    case "yupiter":
+    console.log("Yupiter çox böyük planetdir.");
+    break;
+
+    case "yer":
+    console.log("Yer bizim yaşayış məskənimizdi.");
+    break;
+
+    default:
+    console.log("Yanacaq bu planetə çatmağa kifayət etmədi.");
+}
+
+// 4
+
+let peshe = prompt("Böyüyəndə nə olmaq istəyirsiniz? (həkim, polis, proqramçı, aşpaz)");
+
+switch (peshe) {
+    case "həkim":
+    console.log("İnsanlara kömək edəcəksiniz.");
+    break;
+
+    case "polis":
+    console.log("Ətrafı  qoruyacaqsınız.");
+    break;
+
+    case "proqramçı":
+    console.log("ƏLA proqramlar yazacaqsız.");
+    break;
+
+    case "aşpaz":
+    console.log("Dadlı yeməklər bişirəcəksiniz.");
+    break;
+
+    default:
+    console.log("Ümid edirəm arzunuza çatacaqsınız.");
+}
+// 5
+
+let meyve = prompt("Sevdiyiniz meyvəni yazın (alma, banan, çiyələk, limon)");
+
+switch (meyve) {
+    case "alma":
+    console.log("Rəngi qırmızı və ya yaşıl, dadı şirindir.");
+    break;
+
+    case "banan":
+    console.log("Rəngi sarıdır, dadı şirindir.");
+    break;
+
+    case "çiyələk":
+    console.log("Rəngi qırmızıdır, dadı çox şirindir.");
+    break;
+
+    case "limon":
+    console.log("Rəngi sarıdır, dadı turşdur.");
+    break;
+
+    default:
+    console.log("Bu meyvə səbətdə yoxdur.");
+}
+
+// 6
+
+let heyvan = prompt("Hansı heyvanı yedizdirmək istəyirsiniz? (pişik, dovşan, panda)");
+
+switch (heyvan) {
+    case "pişik":
+    console.log("Pişik balıq yeyir.");
+    break;
+
+    case "dovşan":
+    console.log("Dovşan kök yeyir.");
+    break;
+
+    case "panda":
+    console.log("Panda bambuk yeyir.");
+    break;
+
+    default:
+    console.log("Bu heyvan haqqında məlumat yoxdur.");
+}
+
+// 7
+
+let qehreman = prompt("Hansı cizgi filmi qəhrəmanı ilə görüşmək istəyirsiniz? (şrek, süngər bob, mikki maus)");
+
+switch (qehreman) {
+    case "şrek":
+    console.log("Şrek deyir: Mən öz bataqlığımı sevirəm!");
+    break;
+
+    case "süngər bob":
+    console.log("Süngər Bob həmişə gülümsəyir.");
+    break;
+
+    case "mikki maus":
+    console.log("Mikki Maus hamıya salam verir.");
+    break;
+
+    default:
+    console.log("Bu qəhrəman hazırda tətildədir.");
+}
+// 8
+
+let neqliyyat = prompt("Nə ilə səyahət etmək istəyirsiniz? (gəmi, təyyarə, qatar, qayıq)");
+
+switch (neqliyyat) {
+    case "gəmi":
+    console.log("Bu nəqliyyat dənizdə hərəkət edir.");
+    break;
+
+    case "təyyarə":
+    console.log("Bu nəqliyyat göydə hərəkət edir.");
+    break;
+
+    case "qatar":
+    console.log("Bu nəqliyyat dəmir yolu ilə hərəkət edir.");
+    break;
+
+    default:
+    console.log("Bu nəqliyyat yoxdur. Piyada gəzmək olar.");
+}
---
+++ Dərs 53 JS/sinif task/app.js
@@ -0,0 +1,115 @@
+let ay = Number(prompt("Ayın nömrəsini daxil edin:"));
+
+switch (ay) {
+    case 1:
+    console.log("Yanvar");
+    break;
+
+    case 2:
+    console.log("Fevral");
+    break;
+
+    case 3:
+    console.log("Mart");
+    break;
+
+    case 4:
+    console.log("Aprel");
+    break;
+
+    case 5:
+    console.log("May");
+    break;
+
+    case 6:
+    console.log("İyun");
+    break;
+
+    case 7:
+    console.log("İyul");
+    break;
+
+    case 8:
+    console.log("Avqust");
+    break;
+
+    case 9:
+    console.log("Sentyabr");
+    break;
+
+    case 10:
+    console.log("Oktyabr");
+    break;
+
+    case 11:
+    console.log("Noyabr");
+    break;
+
+    case 12:
+    console.log("Dekabr");
+    break;
+
+    default:
+        console.log("Bu ədəd ilə ay yoxdur");
+}
+
+//2
+let reng = Number(prompt("Svetofor rənginin nömrəsini daxil et:"));
+
+switch (reng) {
+    case 1:
+    console.log("Qırmızı");
+    break;
+
+    case 2:
+    console.log("Sarı");
+    break;
+
+    case 3:
+    console.log("Yaşıl");
+    break;
+
+    default:
+        console.log("Bu ədəd ilə svetofor rəngi yoxdur");
+}
+
+//3
+
+let planet = Number(prompt("Planetin sıra nömrəsini daxil et:"));
+
+switch (planet) {
+    case 1:
+    console.log("Merkuri");
+    break;
+
+    case 2:
+    console.log("Venera");
+    break;
+
+    case 3:
+    console.log("Yer");
+    break;
+
+    case 4:
+    console.log("Mars");
+    break;
+
+    case 5:
+    console.log("Yupiter");
+    break;
+
+    case 6:
+    console.log("Saturn");
+    break;
+
+    case 7:
+    console.log("Uran");
+    break;
+
+    case 8:
+    console.log("Neptun");
+    break;
+
+    default:
+        console.log("Bu ədəd ilə planet yoxdur");
+}
