77ee32f61817c0e618e7c5a7d2bbeb86555f736c
Tunar2026 <bagirlitunar7@gmail.com>
2026-07-06 14:19:02 +0400

    Tasklar

---
+++ Dərs 54 JS/sinif task/app.js
@@ -0,0 +1,11 @@
+// 1 
+function salamDunya() {
+    console.log("Salam dünya!");
+}
+salamDunya();
+
+// 2
+function salamVer() {
+    let ad = prompt("Ad daxil edin:");
+    console.log("Salam, " + ad + "!");
+}
