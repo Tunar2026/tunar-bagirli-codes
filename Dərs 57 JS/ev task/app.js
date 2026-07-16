2540ec66cf7c530c52ea9fe99787d73390b8f299
Tunar2026 <bagirlitunar7@gmail.com>
2026-07-16 10:13:09 +0400

    JS 10 tasklar

---
+++ Dərs 57 JS/ev task/app.js
@@ -0,0 +1,11 @@
+//1
+let massiv = [12, 15, 9, 14, 11, 20];
+let numMax = Math.max(...massiv);
+let numMin = Math.min(...massiv);
+console.log(numMax);
+console.log(numMin);
+
+//2
+let dice1 = Math.floor(Math.random() * 6) + 1;
+let dice2 = Math.floor(Math.random() * 6) + 1;
+console.log(dice1, dice2);
---
+++ Dərs 57 JS/sinif task/app.js
@@ -0,0 +1,17 @@
+//1
+let eliPoints = 120;
+let leylaPoints = 155;
+
+let fərq = Math.abs(eliPoints - leylaPoints);
+
+console.log(fərq);
+
+//2
+let qiymet = 4.6 ;
+let baha = Math.ceil(qiymet);
+let ucuz = Math.floor(qiymet);
+let normal = Math.round(qiymet);
+console.log("baha:" +baha);
+console.log("ucuz:" +ucuz);
+console.log("normal:" +normal);
+
