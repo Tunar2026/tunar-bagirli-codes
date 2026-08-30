beed4c236cf9fbf6a9a2945eb68968ecad04eac5
Tunar2026 <bagirlitunar7@gmail.com>
2026-08-30 13:10:16 +0400

    task

---
+++ Dərs 64 JS/sinif task/app.js
@@ -0,0 +1,25 @@
+let maşın = {
+    brand: "BMW",
+    model: "F30",
+    year: 2015,
+    color: "Qara",
+
+    getCarInfo: function () {
+        return `Maşın: ${this.brand},${this.model}, İl: ${this.year}, Rəng: ${this.color}`;
+    }
+};
+
+console.log(maşın.getCarInfo());
+//2
+
+const student = {
+    name: "Aysel",
+    exam1: 85,
+    exam2: 95,
+
+    calcAverage: function () {
+        return (this.exam1 + this.exam2) / 2;
+    }
+}
+
+console.log(student.calcAverage()); 
